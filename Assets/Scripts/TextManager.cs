8360f2b8c89796c78f6f8676d27c7a45f006909f
Leon Higgin <admin@leonmichaelhiggin>
2021-01-22 19:50:57 +0000

    TextManager and TriggerVolume classes

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    private static TextManager _instance;
+    private TMP_Text _display;
+    private static readonly string[] DisplayMessages = {string.Empty, "I am a BANANA!", "I am not myself today", "I’m in Volume 3, its nice here!"};
+
+    private void Awake()
+    {
+        ResolveDependencies();
+        SetDisplayText();
+    }
+
+    private void ResolveDependencies()
+    {
+        _instance = this;
+        _display = GetComponent<TMP_Text>();
+    }
+
+    public static void SetDisplayText(int index = 0)
+    {
+        _instance._display.text = DisplayMessages[index];
+    }
+}
---
+++ Assets/Scripts/TriggerVolume.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TriggerVolume : MonoBehaviour
+{
+    [SerializeField] private TriggerType triggerType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TextManager.SetDisplayText((int)triggerType);
+    }
+}
+
+public enum TriggerType
+{
+    None = 0,
+    VolumeOne = 1,
+    VolumeTwo = 2,
+    VolumeThree = 3
+}
