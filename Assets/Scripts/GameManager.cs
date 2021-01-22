a3fed975168cd0d9162c7acd8c7cf1072d1f9e44
Leon Higgin <admin@leonmichaelhiggin>
2021-01-22 21:42:54 +0000

    Complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     private static Vector3 _defaultPlayerScale;
+    private static float _defaultPlayerTriggerRadius;
     private static MeshRenderer _playerRenderer;
     private static Transform _playerTransform;
+    private static SphereCollider _playerCollider;
     private static readonly Vector3 SmallPlayerScale = new Vector3(0.5f, 0.5f, 0.5f);
     [SerializeField] private GameObject playerObject;
     [SerializeField] private Material defaultPlayerMaterial;
@@ -23,7 +25,9 @@ public class GameManager : MonoBehaviour
         Instance = this;
         _playerTransform = playerObject.transform;
         _playerRenderer = playerObject.GetComponent<MeshRenderer>();
+        _playerCollider = playerObject.GetComponent<SphereCollider>();
         _defaultPlayerScale = _playerRenderer.transform.localScale;
+        _defaultPlayerTriggerRadius = _playerCollider.radius;
     }
 
     public void PlayerEnteredTrigger(TriggerType triggerType = TriggerType.None)
@@ -35,6 +39,7 @@ public class GameManager : MonoBehaviour
             case TriggerType.None:
                 _playerRenderer.material = defaultPlayerMaterial;
                 _playerTransform.localScale = _defaultPlayerScale;
+                _playerCollider.radius = _defaultPlayerTriggerRadius;
                 break;
             case TriggerType.VolumeOne:
                 _playerRenderer.material = yellowMaterial;
@@ -44,6 +49,9 @@ public class GameManager : MonoBehaviour
                 break;
             case TriggerType.VolumeThree:
                 _playerTransform.localScale = SmallPlayerScale;
+                //POTENTIAL BUG
+                //As the player leaves the trigger, there sphere is now bigger and there for is BACK IN TRIGGER because of its bigger size!
+                _playerCollider.radius *= 2; //This stops that from happening but means the player stays small for an area outside of the trigger.
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(triggerType), triggerType, null);
